849f5eeb8589208384fa3cb063cc6d7a4c44a64c
Briann Popson <bpopson@gmail.com>
2016-02-26 08:28:01 -0800

    create get and set methods for stylist properties

--- src/Stylist.php
+++ src/Stylist.php
@@ -14,6 +14,41 @@ class Stylist
         $this->id = $id;
     }
 
+    function setName($new_name)
+    {
+        $this->name = (string) $new_name;
+    }
+
+    function getName()
+    {
+        return $this->name;
+    }
+
+    function setSpecialty($new_specialty)
+    {
+        $this->specialty = (string) $new_specialty;
+    }
+
+    function getSpecialty()
+    {
+        return $this->specialty;
+    }
+
+    function setEmail($new_email)
+    {
+        $this->email = (string) $new_email;
+    }
+
+    function getEmail()
+    {
+        return $this->email;
+    }
+
+    function getId()
+    {
+        return $this->id;
+    }
+
 
   }
 ?>
