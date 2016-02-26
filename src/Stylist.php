30b625064f07776b0e6c0093c1b319088ef68474
Briann Popson <bpopson@gmail.com>
2016-02-26 08:31:47 -0800

    create save, getAll, and deleteAll functions for Stylist

--- src/Stylist.php
+++ src/Stylist.php
@@ -49,6 +49,31 @@ class Stylist
         return $this->id;
     }
 
+    function save()
+    {
+        $GLOBALS['DB']->exec("INSERT INTO hair_salon (name, specialty, email) VALUES ('{$this->getName()}', '{$this->getSpecialty()}', '{$this->getEmail()}')");
+        $this->id = $GLOBALS['DB']->lastInsertId();
+    }
+
+    static function getAll()
+    {
+        $returned_stylists = $GLOBALS['DB']->query("SELECT * FROM hair_salon;");
+        $stylists = array();
+        foreach($returned_stylists as $stylist) {
+            $name = $stylist['name'];
+            $specialty = $stylist['specialty'];
+            $email = $stylist['email'];
+            $id = $stylist['id'];
+            $new_stylist = new Restaurant($name, $specialty, $email, $id);
+            array_push($stylists, $new_stylist);
+        }
+        return $stylists;
+    }
+
+    static function deleteAll()
+    {
+        $GLOBALS['DB']->exec("DELETE FROM hair_salon;");
+    }
 
   }
 ?>
