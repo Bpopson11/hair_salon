2fe5aa6ce9432400d78d09415e8e70c45a7513e8
Briann Popson <bpopson@gmail.com>
2016-02-26 10:50:00 -0800

    set up basic methods for Client.php

--- src/Client.php
+++ src/Client.php
@@ -32,6 +32,48 @@ class Client
         $this->id = $GLOBALS['DB']->lastInsertId();
     }
 
+    static function getAll()
+    {
+        $returned_clients = $GLOBALS['DB']->query("SELECT * FROM clients");
+        $clients = array();
+        foreach($returned_clients as $client) {
+            $name = $client['name'];
+            $id = $client['id'];
+            $new_client = new Client($name, $id);
+            array_push($clients, $new_client);
+        }
+        return $clients;
+    }
+
+    static function deleteAll()
+    {
+        $GLOBALS['DB']->exec("DELETE FROM clients;");
+    }
+
+    function deleteClient()
+    {
+        $GLOBALS['DB']->exec("DELETE FROM clients WHERE id = {$this->getId()};");
+    }
+
+    function updateClientName($new_name)
+    {
+        $GLOBALS['DB']->exec("UPDATE clients SET name = '{$new_name}' WHERE id = {this->getID()};");
+        $this->setName($new_name);
+    }
+
+    static function findClient($search_id)
+    {
+        $found_client = null;
+        $clients = Client::getAl();
+        foreach($clients as $client) {
+            $client_id = $client->getId();
+            if ($client_id == $search_id) {
+                $found_client = $client;
+            }
+        }
+        return $found_client;
+    }
+
 
 }
 
